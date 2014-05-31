670b972fdd6e097026b8a2d6fee789eee910371d
Nikhil Thomas <nikhil@wetfeet.com>
2014-05-31 17:52:56 -0400

    stuff and stuff

---
+++ app/controllers/breeds_controller.rb
@@ -0,0 +1,36 @@
+class BreedsController < ApplicationController
+  respond_to :json
+
+  def index
+    options = params.select {|k, v| %w(size breed sex age offset count).include? k}
+    pets = petfinder.find_pets('dog', params[:location], options)
+    @pets = []
+    pets.each do |pet|
+      next unless pet.sex == options["sex"]
+      
+      @pets << {
+        photos: pet.photos,
+        name: pet.name,
+        id: pet.id,
+        mix: pet.mix,
+        age: pet.age,
+        shelter_id: pet.shelter_id,
+        shelter_pet_id: pet.shelter_pet_id,
+        sex: pet.sex,
+        size: pet.size,
+        description: pet.description,
+        breeds: pet.breeds,
+        status: pet.status,
+        options: pet.options
+      }  
+    end
+    
+    render json: @pets
+  end
+
+  def show
+    options = params.keep_if {|p| [:size, :breed, :sex, :age, :offset, :count].include? p}
+    @pets = petfinder.find_pets('dog', params[:location], options)
+    # binding.pry
+  end
+end
---
+++ config/initializers/petfinder.rb
@@ -0,0 +1,4 @@
+Petfinder.configure do |config|
+  config.api_key = 'ba13722a78b94d3d56f9c4e8d26ce68f'
+  config.api_secret = '79f61f27643a9a8c2fe3670a670fdd6c'
+end
